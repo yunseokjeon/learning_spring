af92bb51baee771c3ff73ca994ce7a106348a147
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2021-10-19 18:57:54 +0900

    learning spring 1019

--- KIM/JPA/JPA1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ KIM/JPA/JPA1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class OrderItem extends BaseEntity{
 
@@ -10,7 +12,7 @@ public class OrderItem extends BaseEntity{
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
