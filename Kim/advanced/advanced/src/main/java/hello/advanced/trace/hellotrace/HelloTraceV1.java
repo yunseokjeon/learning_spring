d913960384232ac20a9e2c9848506ea565f675bf
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2022-01-19 14:33:02 +0900

    learning spring 0119

--- Kim/advanced/advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
+++ Kim/advanced/advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
@@ -38,7 +38,11 @@ public class HelloTraceV1 {
             log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
         }
     }
-
+    //level=0
+    //level=1   |-->
+    //level=2   |   |-->
+    //level=2 ex  |     |<X-
+    //level=1 ex  |<X-
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
